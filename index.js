59674414e3beee51504989503f10c2ede5969b8b
Olha <o.v.shevel@gmail.com>
2021-10-01 09:09:58 +0300

    fixes2

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ console.log('Hello world');
 function sum() {
   console.log(4 + 5);
 }
+
+const b = 10;
+if (b === 0) {
+  //doSomething();
+} else if (b === 1) {
+  //doSomethingElse();
+}
+
 sum();
